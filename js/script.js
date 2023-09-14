62a70f2e0619cd6bba27a6f75b5fd978bd9f06ea
Danil <makarovdanil289@gmail.com>
2023-09-14 14:17:09 +0500

    was added interactive timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
+
+    // tabs
+
      let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,4 +36,56 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    // timer
+
+    let deadline = '2023-09-15';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) -  Date.parse(new Date()),
+            seconds = Math.floor((t/1000) %  60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            // hours = Math.floor((t/1000/60/60) % 24)
+            // days = Math.floor((t/(1000*60*60*24)))
+        
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            }
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
