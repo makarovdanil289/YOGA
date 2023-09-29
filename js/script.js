95c3ea509ac3da6fc302ff56a18811b125b06683
Danil <makarovdanil289@gmail.com>
2023-09-29 12:20:17 +0500

    was added calc

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // timer
 
-    let deadline = '2023-09-28';
+    let deadline = '2023-10-28';
 
     function getTimeRemaining(endTime) {
         let t = Date.parse(endTime) -  Date.parse(new Date()),
@@ -221,6 +221,52 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if(restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else if (persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if(persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else if (restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if(restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 });
 
 
